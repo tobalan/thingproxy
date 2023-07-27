b5a1398170844c965267e86f5d8f842ada83936b
tobalan <127816009+tobalan@users.noreply.github.com>
2023-07-28 03:31:48 +0530

    Update server.js

--- server.js
+++ server.js
@@ -70,7 +70,7 @@ function processRequest(req, res) {
         var remoteURL;
 
         try {
-            console.log(result)
+            console.log(result[1])
             remoteURL = url.parse(decodeURI(result[1]),true,true);
             console.log(remoteURL)
         }
